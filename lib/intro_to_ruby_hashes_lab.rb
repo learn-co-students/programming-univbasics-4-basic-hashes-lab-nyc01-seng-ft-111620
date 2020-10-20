cb3cade0efea68045b8d07b3aabf3ab0144b641f
Jakob Persons <jakobep24@gmail.com>
2020-10-20 18:15:19 +0000

    Done.

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,16 +1,27 @@
 def new_hash
   # return an empty hash
+  hash = {}
 end
 
 def my_hash
   # return a valid hash with any key/value pair of your choice
+  jakes_people = {
+    name: "Brynn",
+    relationship: "Fiance"
+  }
 end
 
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  pioneer = {
+    :name => "Grace Hopper"
+  }
 end
 
 
 def id_hash_generator(number)
   # return a hash with a key :id assigned to the provided number
+  hash = {
+    :id => number
+  }
 end
